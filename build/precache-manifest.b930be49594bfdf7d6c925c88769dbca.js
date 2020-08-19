f41f95f301afaf440da5d245be8cc574a32ed04b
DimaNotebook <dima1parshin@gmail.com>
2020-08-19 13:36:47 +0300

    add build

---
+++ build/precache-manifest.b930be49594bfdf7d6c925c88769dbca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "536b43d408a4f3016f73dc8d874c2e21",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8507de754e050d650a39",
+    "url": "./static/css/main.dddb2af4.chunk.css"
+  },
+  {
+    "revision": "f1b33676bee020b2cd13",
+    "url": "./static/js/2.1d200dc3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.1d200dc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8507de754e050d650a39",
+    "url": "./static/js/main.56abb010.chunk.js"
+  },
+  {
+    "revision": "cbc8410088f1e09eff11",
+    "url": "./static/js/runtime-main.1bbe25c3.js"
+  },
+  {
+    "revision": "47563a4a78627b8f576cadc01812e2db",
+    "url": "./static/media/trash.47563a4a.svg"
+  }
+]);
