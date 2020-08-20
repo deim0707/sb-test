0b9a72610ba0a673272b60a4a08405091328e7af
Dmitry <dima@and.com>
2020-08-20 15:29:19 +0300

    reload build

---
+++ build/precache-manifest.535fbb62914a3492aeb6a55982356268.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cd6e42287baa6284fc2c5153e1faf7b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e5344eb1398c5aa85889",
+    "url": "./static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "7ec001ffc3b9332466c2",
+    "url": "./static/css/main.6b825e1d.chunk.css"
+  },
+  {
+    "revision": "e5344eb1398c5aa85889",
+    "url": "./static/js/2.8cd21bc4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.8cd21bc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ec001ffc3b9332466c2",
+    "url": "./static/js/main.e16efa19.chunk.js"
+  },
+  {
+    "revision": "cbc8410088f1e09eff11",
+    "url": "./static/js/runtime-main.1bbe25c3.js"
+  },
+  {
+    "revision": "47563a4a78627b8f576cadc01812e2db",
+    "url": "./static/media/trash.47563a4a.svg"
+  }
+]);
